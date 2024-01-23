ad3610a7be181c2f90f9f9220e158e47ca1b3e93
LuisGomes18 <luiscgomes0@gmail.com>
2024-01-23 17:28:30 +0000

    Adição do gui test

--- test/gui/main_com_gui.py
+++ test/gui/main_com_gui.py
@@ -1,16 +1,33 @@
-from tkinter import *
-from tkinter import ttk
+from tkinter import *  # ANCHOR - Lista de modulos
 
 
+# SECTION - Aolicação
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.titulo()
+        self.subtitulo()
+
+    #ANCHOR -  Definição do titulo
+    def titulo(self):
+        title = Label(
+            self.master, text="TO-DO Python", font=("Helvetica", 14, "bold")
+        )  # TODO Trocar letra
+        title.pack()
+
+    #ANCHOR - Definição do subtitulo
+    def subtitulo(self):
+        subtitulo = Label(
+            self.master,
+            text="Feito em Python 3.12",
+            font=("Helvetica", 10, "bold"),  # TODO Trocar letra
+        )
+        subtitulo.pack()
+# !SECTION
+
+
+# SECTION - Rodar a app
 root = Tk()
-frm = ttk.Frame(root, padding=20)
-frm.grid()
-
-root.title('TO-DO APP')
-root.geometry("400x200")
-
-ttk.Label(frm, text="TO-DO App").grid(column=0, row=0)
-ttk.Button(frm, text="Sair", command=root.destroy).grid(column=1, row=0)
-
-
+app = App(root)
 root.mainloop()
+# !SECTION
