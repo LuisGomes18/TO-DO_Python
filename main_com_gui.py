e9b48b1c362e24a8c595379b9c372a624c20f3d4
LuisGomes18 <luiscgomes0@gmail.com>
2024-02-19 17:22:01 +0000

    Update do main com gui.py

--- main_com_gui.py
+++ main_com_gui.py
@@ -35,10 +35,31 @@ def salvar(id, titulo, descricao):
     guardar_cartoes(Cartoes)
 
 
+def apagar_todo(self):
+    id_ticket = int(self.id_apagar.get())
+    if id_ticket not in self.IDs:
+        messagebox.showerror("Erro", "ID do Ticket não encontrado.")
+        return
+
+    apagar_mesmo = messagebox.askquestion("Apagar Ticket", "Tem certeza que deseja apagar esse Ticket?")
+    if apagar_mesmo == "yes":
+        Cartoes.pop(id_ticket, None)
+        self.IDs.remove(id_ticket)
+        self.dados_IDs['IDs'] = self.IDs
+        guardar_ids(self.dados_IDs)
+        guardar_cartoes(Cartoes)
+        self.id_apagar.delete(0, 'end')
+        messagebox.showinfo("Sucesso", "Ticket apagado com sucesso.")
+    else:
+        self.id_apagar.delete(0, 'end')
+        messagebox.showinfo("Sucesso", "Ticket não apagado.")
+
+
 class Application:
     def __init__(self, master=None):
         self.master = master
-        self.IDs = carregar_ids()
+        self.dados_IDs = carregar_ids()
+        self.IDs = self.dados_IDs['IDs']
         self.tela()
 
     def destroy_window(self):
@@ -53,14 +74,42 @@ class Application:
         self.master.title("TODO Python")
         self.master.geometry("600x600+250+50")
 
-        titulo = Label(self.master, text='TODO Python')
+        tela_1 = Frame(self.master)
+        tela_1["pady"] = 10
+        tela_1.pack()
+
+        tela_2 = Frame(self.master)
+        tela_2["pady"] = 10
+        tela_2.pack()
+
+        tela_3 = Frame(self.master)
+        tela_3.pack()
+
+        tela_4 = Frame(self.master)
+        tela_4["pady"] = 40
+        tela_4.pack()
+
+        titulo = Label(tela_1, text='TODO Python')
         titulo["font"] = ("Arial", 20, "bold")
         titulo.pack()
 
-        botao_criar = Button(self.master, text='Criar', command=self.criar)
+        botao_criar = Button(tela_2, text='Criar', command=self.criar)
+        botao_criar["font"] = ("Arial", 10, "bold")
+        botao_criar["fg"] = "black"
+        botao_criar["bg"] = "white"
         botao_criar.pack()
-        botao_apagar = Button(self.master, text='Apagar', command=self.apagar)
+
+        botao_apagar = Button(tela_3, text='Apagar', command=self.tela_apagar)
+        botao_apagar["font"] = ("Arial", 10, "bold")
+        botao_apagar["fg"] = "black"
+        botao_apagar["bg"] = "white"
         botao_apagar.pack()
+
+        botao_sair = Button(tela_4, text='Sair', command=self.destroy_window)
+        botao_sair["font"] = ("Arial", 10, "bold")
+        botao_sair["fg"] = "black"
+        botao_sair["bg"] = "white"
+        botao_sair.pack()
 
     def criar(self):
         data_hoje = datetime.date.today()
@@ -93,6 +142,10 @@ class Application:
         tela_5 = Frame(self.master)
         tela_5["pady"] = 10
         tela_5.pack()
+
+        tela_6 = Frame(self.master)
+        tela_6["pady"] = 10
+        tela_6.pack()
 
         titulo_ticket_label = Label(tela_1, text='Título do Ticket')
         titulo_ticket_label["font"] = ("Arial", 10, "bold")
@@ -132,6 +185,13 @@ class Application:
         botao_salvar["width"] = 6
         botao_salvar.pack()
 
+        botao_voltar = Button(tela_6, text='Voltar', command=self.tela)
+        botao_voltar["font"] = ("Arial", 10, "bold")
+        botao_voltar["fg"] = "black"
+        botao_voltar["bg"] = "white"
+        botao_voltar["width"] = 6
+        botao_voltar.pack()
+
     def salvar_dados(self):
         titulo = self.titulo_ticket.get()
         descricao = self.descricao_ticket.get("1.0", "end-1c")
@@ -144,13 +204,14 @@ class Application:
         self.descricao_ticket.delete("1.0", "end")
         self.tela()
 
-    def gerar_novo_id(self):
+    @staticmethod
+    def gerar_novo_id():
         id = randint(10000, 99999)
         while id in IDs:
             id = randint(10000, 99999)
         return id
 
-    def apagar(self):
+    def tela_apagar(self):
         self.destroy_window()
         self.master = Tk()
         self.master.title("Apagar Ticket")
@@ -172,6 +233,9 @@ class Application:
         self.tela_apagar_4["pady"] = 10
         self.tela_apagar_4.pack()
 
+        self.tela_apagar_5 = Frame(self.master)
+        self.tela_apagar_5.pack()
+
         self.titulo = Label(self.tela_apagar_1, text='Apagar Ticket')
         self.titulo["font"] = ("Arial", 20, "bold")
         self.titulo.pack()
@@ -185,23 +249,19 @@ class Application:
         self.id_apagar["width"] = 50
         self.id_apagar.pack()
 
-        self.botao_apagar = Button(self.tela_apagar_4, text='Apagar', command=self.apagar_ticket)
+        self.botao_apagar = Button(self.tela_apagar_4, text='Apagar', command=lambda: apagar_todo(self))
         self.botao_apagar["font"] = ("Arial", 10, "bold")
         self.botao_apagar["fg"] = "black"
         self.botao_apagar["bg"] = "white"
         self.botao_apagar["width"] = 6
         self.botao_apagar.pack()
 
-    def apagar_ticket(self):
-        id = self.id_apagar.get()
-        if id not in IDs:
-            messagebox.showerror("Erro", "ID do Ticket não encontrado.")
-            return
-        Cartoes.pop(id, None)
-        IDs.remove(id)
-        dados_IDs['IDs'] = IDs
-        guardar_ids(dados_IDs)
-        self.id_apagar.delete(0, 'end')
+        self.botao_voltar = Button(self.tela_apagar_5, text='Voltar', command=self.tela)
+        self.botao_voltar["font"] = ("Arial", 10, "bold")
+        self.botao_voltar["fg"] = "black"
+        self.botao_voltar["bg"] = "white"
+        self.botao_voltar["width"] = 6
+        self.botao_voltar.pack()
 
 
 def main():
